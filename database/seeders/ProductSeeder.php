e760b25ba12706df7048b8f2af86e2f876072c4b
Esen Meredow <esca6585@gmail.com>
2024-10-15 12:47:18 +0500

    Update from Artisan command - inside data updated

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,26 +2,43 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
 use Illuminate\Database\Seeder;
-use App\Models\Image;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class ProductSeeder extends Seeder
 {
     public function run()
     {
-        $chunkSize = 10;
-        $totalProducts = 50;
+        $faker = Faker::create();
 
-        for ($i = 0; $i < $totalProducts; $i += $chunkSize) {
-            $chunk = min($chunkSize, $totalProducts - $i);
-            
-            DB::transaction(function () use ($chunk) {
-                Product::factory()->count($chunk)->create()->each(function ($product) {
-                    $product->images()->saveMany(Image::factory()->count(5)->make());
-                });
-            });
+        // Assuming you have shops and categories already seeded
+        $shopIds = DB::table('shops')->pluck('id')->toArray();
+        $categoryIds = DB::table('categories')->pluck('id')->toArray();
+
+        foreach (range(1, 50) as $index) {  // Create 50 products
+            DB::table('products')->insert([
+                'name' => $faker->words(3, true),
+                'price' => $faker->randomFloat(2, 10, 1000),
+                'discount' => $faker->optional(0.3)->numberBetween(5, 50),
+                'description' => $faker->paragraph,
+                'gender' => $faker->randomElement(['Men', 'Women', 'Children']),
+                'sizes' => json_encode($faker->randomElements(range(42, 50), $faker->numberBetween(1, 5))),
+                'separated_sizes' => json_encode($faker->randomElements(['S', 'M', 'L', 'XL', 'XXL'], $faker->numberBetween(1, 5))),
+                'color' => $faker->safeColorName,
+                'manufacturer' => $faker->country,
+                'width' => $faker->randomFloat(2, 10, 100),
+                'height' => $faker->randomFloat(2, 10, 100),
+                'weight' => $faker->numberBetween(100, 5000),  // in grams
+                'production_time' => $faker->numberBetween(60, 1440),  // in minutes (1 hour to 24 hours)
+                'min_order' => $faker->optional()->numberBetween(1, 10),
+                'seller_status' => $faker->boolean,
+                'status' => $faker->boolean,
+                'shop_id' => $faker->randomElement($shopIds),
+                'category_id' => $faker->randomElement($categoryIds),
+                'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
+                'updated_at' => $faker->dateTimeBetween('-1 year', 'now'),
+            ]);
         }
     }
 }
